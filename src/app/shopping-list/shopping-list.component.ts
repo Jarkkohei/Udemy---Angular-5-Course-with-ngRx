655105619758ee5e548b970b5b732d49a37295e6
Jarkko Heikkinen <jarkkohei@luukku.com>
2017-12-14 11:31:23 +0200

    Changed the getting the ingredients to use the state instead of the subscription

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -3,6 +3,8 @@ import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 import { Subscription } from 'rxjs/Subscription';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-shopping-list',
@@ -10,20 +12,23 @@ import { Subscription } from 'rxjs/Subscription';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[];
+
+  shoppingListState: Observable<{Ingredients: Ingredient[]}>;
 
   private subscription: Subscription;
 
-  constructor(private slService: ShoppingListService) {}
+  constructor(
+    private slService: ShoppingListService,
+    private store: Store<{shoppingList: {Ingredients: Ingredient[]}}>) {}
 
   ngOnInit() {
-    this.ingredients = this.slService.getIngredients();
-    this.subscription = this.slService.ingredientsChanged
-      .subscribe(
-        (ingredients: Ingredient[]) => {
-          this.ingredients = ingredients;
-        }
-      );
+    this.shoppingListState = this.store.select('shoppingList');
+    //this.subscription = this.slService.ingredientsChanged
+    //  .subscribe(
+    //    (ingredients: Ingredient[]) => {
+    //      this.ingredients = ingredients;
+    //    }
+    //  );
   }
 
   onEditItem(index: number) {

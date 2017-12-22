1f44b48acc7413277865a2cb743bb603be78f3a3
Jarkko Heikkinen <jarkkohei@luukku.com>
2017-12-22 17:04:44 +0200

    Added the StoreRouterConnectingModule to the AppModule

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -2,10 +2,8 @@ import { Action } from '@ngrx/store';
 
 export const TRY_SIGNUP = 'TRY_SIGNUP';
 export const SIGNUP = 'SIGNUP';
-
 export const TRY_SIGNIN = 'TRY_SIGNIN';
 export const SIGNIN = 'SIGNIN';
-
 export const LOGOUT = 'LOGOUT';
 export const SET_TOKEN = 'SET_TOKEN';
 
@@ -38,6 +36,7 @@ export class SetToken implements Action {
 
     constructor(public payload: string) {}
 }
+
 
 export type AuthActions = 
     Signup | 
